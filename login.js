a07b40f076ad0f2b0aac075f60b7523af55610a0
thesebas <thesebas@thesebas.net>
2016-01-15 10:43:19 +0100

    cleanup, start some docs

--- login.js
+++ login.js
@@ -1,101 +1,16 @@
-var request = require('request-promise');
-var crypto = require('crypto');
-var Promise = require('bluebird');
-
 var {username , password} = require('./stuff/passwd');
-var token = '';
 
-function callApi (method, data, trace) {
-	var body = {locale: 'PL',  token: token, trace: trace || ''};
-
-	if (data) {
-		body.data = data;
-	}
-
-	return request({
-		url: 'https://login.ingbank.pl/mojeing/rest/' + method,
-		method: 'POST',
-		json: true,
-		jar: true,
-		headers: {
-			'Content-type': 'application/json',
-			'X-Wolf-Protection': Math.random()
-		},
-		forever: true,
-		body: body
-	}).then(function (resp) {
-		if (resp.status != 'OK') {
-			throw new Error('METHOD ERROR :' + method, resp)
-		}
-		if (resp.data && resp.data.token) {
-			token = resp.data.token;
-		}
-		return resp.data;
-	}, function (err) {
-		console.error('REQUEST ERROR');
-		throw err;
-	});
-}
-
-function checklogin (login) {
-	return callApi('renchecklogin', {login: login});
-}
-
-function login (prelogindata, login, passwd) {
-	var mixedpasswd = mixFullPasswdMask(prelogindata.mask, prelogindata.salt, passwd);
-	var pwdhash = hashPassword(prelogindata.key, mixedpasswd);
-	return callApi('renlogin', {pwdhash: pwdhash, login: login});
-}
-
-function mixShortPasswdMask (mask, salt, passwd) {
-	passwd = passwd.split('');
-	salt = salt.split('');
-	return mask.split('').map(function (i, k) {
-		return i === '+' ? salt[k]: passwd.shift();
-	}).join('');
-}
-
-function mixFullPasswdMask (mask, salt, passwd) {
-	passwd = passwd.split('');
-	salt = salt.split('');
-	return mask.split('').map(function (i, k) {
-		return i === '+' ? salt[k]: passwd[k];
-	}).join('');
-}
-
-function hashPassword (key, passwordmix) {
-	var hmac = crypto.createHmac('sha1', key);
-
-	hmac.setEncoding('hex');
-	hmac.write(passwordmix);
-	hmac.end();
-
-	return hmac.read();
-}
+var mojeing = require('./lib/mojeing')();
 
 function dumpJson (data) {
 	console.log(JSON.stringify(data, null, ' '));
 }
 
-Promise.resolve()
-	.then(function () {return checklogin(username);})
-	.then(function (prelogindata) {return login(prelogindata, username, password);})
+mojeing.authorize(username , password)
+	.then(function () {
+		return mojeing.callApi('rengetallaccounts');
+	})
 	.then(function (resp) {
-		return Promise.resolve()
-			.then(function () {return callApi('rengetallaccounts');})
-			.then(function (resp) {
-				dumpJson(resp);
-			});
-	}).catch(function (err) {
-	console.error('uncatched err');
-	console.error(err);
-});
-
-// methods with (token, locale)
-// rengetmainpageprds ()
-// rengetnotification ({scmode: "D"})
-// rengetallaccounts ()
-// rengetinv ()
-// rengetallcards ()
-// rengetproperties ({prefix: "LIQUID"|"CONFIG"|"LOGIN"|"ADVERT"})
-// rengetfury ({fromDate: "2015-07-13", toDate: "2016-01-13", rach: [], search: "", maxTrn: 34, skipTrn: 0, maxsug: 0})
+		dumpJson(resp);
+		console.log(mojeing.getToken());
+	});
